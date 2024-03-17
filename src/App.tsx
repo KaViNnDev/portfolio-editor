2d32495c60b09b80f565597255d8eaecca161181
Kavinkumar <kavin.n.dev@gmail.com>
2024-03-18 02:16:04 +0530

    feat: Grid System Added to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
-import { ThemeProvider } from '@mui/material';
+import { Container, ThemeProvider } from '@mui/material';
 import { theme } from './Theme/theme';
 import { NavBar } from './components/NavBar/NavBar';
+import { GridSections } from './Sections/GridSections';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <NavBar />
+      <Container sx={{ padding: '47px 0px' }}>
+        <GridSections />
+      </Container>
     </ThemeProvider>
   );
 }
